b9bed831a5040d9ef806d59a969a3a5c06305c0b
samuelbahri <samuel.rbahri@gmail.com>
2021-04-04 00:15:55 -0400

    Added one more test for response status code

--- webapp_unittest.py
+++ webapp_unittest.py
@@ -7,6 +7,11 @@ class SampleTestCase(unittest.TestCase):
     def test_response(self):
         tester = app.test_client(self)
         response = tester.get('/foundme')
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_data(self):
+        tester = app.test_client(self)
+        response = tester.get('/foundme')
         self.assertEqual(response.data, b'You found me!')
 
 if __name__ == '__main__':
